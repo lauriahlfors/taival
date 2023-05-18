0961a1ce25c95a60b2cff55abe32fe841d3ea3b1
Lauri Ahlfors <ahlfroslauri@gmail.com>
2023-05-18 14:11:19 +0300

    feat: add Prettier configuration

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  endOfLine: 'lf',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  // Prettier plugins
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
+  // Disable plugin auto-loading
+  pluginSearchDirs: false,
+};
