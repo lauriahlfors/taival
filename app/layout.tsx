4c3794bad74f7c581ad6c1409212f0d31176b8be
Lauri Ahlfors <ahlfroslauri@gmail.com>
2023-05-18 14:15:59 +0300

    feat: add Manrope font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,24 @@
-import './globals.css'
+import { Manrope } from 'next/font/google';
+import './globals.css';
+
+const manrope = Manrope({
+  weight: ['400'],
+  subsets: ['latin'],
+});
 
 export const metadata = {
   title: 'Taival',
   description: 'A travel planning app!',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={manrope.className}>
       <body>{children}</body>
     </html>
-  )
+  );
 }
