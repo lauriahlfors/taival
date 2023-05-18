9ae217615d3dd7672a64b464ec0884c4f0e0b647
Lauri Ahlfors <ahlfroslauri@gmail.com>
2023-05-18 15:38:20 +0300

    feat: add GitHub and Google providers

--- lib/next-auth-options.ts
+++ lib/next-auth-options.ts
@@ -1,6 +1,9 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { NextAuthOptions } from 'next-auth';
 
+import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+
 import prisma from './prisma-client';
 
 export const nextAuthOptions: NextAuthOptions = {
@@ -8,5 +11,14 @@ export const nextAuthOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt',
   },
-  providers: [],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
 };
